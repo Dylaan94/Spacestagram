2062ae90290cf66d8f96466be57ccdc75b4665fc
Dylan Brain <dylanbrain@googlemail.com>
2022-01-13 14:46:43 +0000

    nasaAPI returns data

--- api/routes/nasaAPI.js
+++ api/routes/nasaAPI.js
@@ -1,14 +1,24 @@
-let express = require("express")
+let express = require("express");
 let router = express.Router();
-let axios = require("axios")
-
-
+let axios = require("axios");
+require("dotenv").config();
 
 router.get("/", (req, res, next) => {
-    res.send("API is working yessss")
-    axios.get("https://api.nasa.gov/planetary/apod");
-})
+  // res.send("API is working yessss")
+  axios
+    .get(
+      "https://api.nasa.gov/planetary/apod?api_key=" + process.env.NASA_API_KEY + ""
+    )
+    .then((response) => {
+      let imageObject = {
+        title: response.data.title,
+        date: response.data.data,
+        explanation: response.data.explanation,
+        copyright: response.data.copyright,
+        imageURL: response.data.url,
+      };
+        res.send(imageObject);
+    });
+});
 
 module.exports = router;
-
-//https://api.nasa.gov/planetary/apod
